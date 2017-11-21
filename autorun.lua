ab07b87dc6ca11fc69e8ca29da6d4ec9057930b4
kha'Cey Salvador <khacey.salvador@gmail.com>
2017-11-21 16:56:04 +0000

    charts lua added

--- autorun.lua
+++ autorun.lua
@@ -1,2 +1,3 @@
+os.execute("wget -f https://raw.githubusercontent.com/OpenPrograms/Fingercomp-Programs/master/charts/charts.lua charts.lua")
 os.execute("wget -f https://dl.dropbox.com/s/b4b5ecgvxbpic5u/reactorControl.lua energymanager.lua")
 os.execute("energymanager")
--- reactorControl.lua
+++ reactorControl.lua
@@ -22,10 +22,10 @@ if component.isAvailable("block_refinedstorage_interface") then
 	storage = component.block_refinedstorage_interface
 else print("There is no Inventory Network detected!") proceed = false
 end
---if component.isAvailable("induction_matrix") then
---	battery = component.induction_matrix
---else print("There is no Battery detected!") proceed = false
---end
+if component.isAvailable("induction_matrix") then
+	battery = component.induction_matrix
+else print("There is no Battery detected!") proceed = false
+end
 for address, name in component.list() do
 	if name == "br_reactor" then
 		local reactor = component.proxy(address)
@@ -40,7 +40,35 @@ local energy = 0
 local controlRodLevels = 0
 local rfpertick = 0
 local fuelLifeSpan = 0
-
+local input = 0
+local output = 0
+--charts
+local bar = charts.Container {
+  x = 2,
+  y = 4,
+  bg = 0xffffff,
+  width = 61,
+  height = 3,
+  payload = charts.ProgressBar {
+    direction = charts.sides.RIGHT,
+    value = 0,
+    colorFunc = function(_, perc)
+      if perc >= .9 then
+        return 0x20afff
+      elseif perc >= .75 then
+        return 0x20ff20
+      elseif perc >= .5 then
+        return 0xafff20
+      elseif perc >= .25 then
+        return 0xffff20
+      elseif perc >= .1 then
+        return 0xffaf20
+      else
+        return 0xff2020
+      end
+    end
+  }
+}
 --functions
 local function getUranium()
 	local size = 0
@@ -81,7 +109,18 @@ while proceed do
 		term.clear()
 		gpu.set(1, 2, ("Fuel Life Span: " .. fuelLifeSpan))
 	end
+	term.clear()
+	bar.gpu.set(2, 1, "Energy Capacity Monitor")
+	bar.gpu.set(2, 3, "Main RF Network")
+	bar.gpu.set(31, 7, ("%.1f"):format(bar.payload.value*100.0) .. "%");
+	bar.gpu.set(5, 8, "INFO")
+	bar.gpu.set(5, 9, "Capacity : " .. ("%.0f"):format(energy) .. " / " .. energyCapacity)
+	bar.gpu.set(5, 10, "Input    : " .. ("%.0f"):format(input) .. "RF/t")
+	bar.gpu.set(5, 11, "Output   : " .. ("%.0f"):format(output) .. "RF/t")
+	bar.gpu.set(5, 12, "Excess   : " .. ("%.0f"):format(input - output) .. "RF/t")
 
+	bar.payload.value =  (energy / energyCapacity)
+	bar:draw()
 	if event.pull(0.05, "interrupted") then
 		term.clear()
 		os.exit()
